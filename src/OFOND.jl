69bbc33fb9ca585faaeaf85fec6f254e36e77348
mathbri <mathis.brichet@gmail.com>
2024-02-28 11:29:52 +0100

    Adding graphs files

--- src/OFOND.jl
+++ src/OFOND.jl
@@ -2,14 +2,20 @@ module OFOND
 
 # Packages needed across the project
 
+using Graphs, MetaGraphsNext
+
 # Project files
 
 include("Constants.jl")
 include("Utils.jl")
 
 # Structures 
+include("Structures/Network.jl")
+include("Structures/TravelTime.jl")
+include("Structures/Bin.jl")
+include("Structures/TimeSpace.jl")
 
-# Reading and Writing
+# Import and Export of data
 
 # Algorithms
 
