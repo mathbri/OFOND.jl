d3ede0fc8386a63473545d36b8d6311ef9ea9a01
mathbri <mathis.brichet@gmail.com>
2024-03-14 10:27:22 +0100

    Finishing Bundle-Order-Commodity structures

--- src/Structures/Commodity.jl
+++ src/Structures/Commodity.jl
@@ -2,8 +2,20 @@
 
 struct Commodity
     order :: Order        # order of the commodity
-    partNumber :: String  # part number of the commodity (still string because need it)
-    packageSize :: Int  # size of one package in m3 / 100 
+    partNumber :: String  # part number of the commodity
+    size :: Int           # size of one package in m3 / 100 
 end
 
 # Methods
+
+function get_supplier(commodity::Commodity)
+    return commodity.order.bundle.supplier
+end
+
+function get_customer(commodity::Commodity)
+    return commodity.order.bundle.customer
+end
+
+function get_delivery_date(commodity::Commodity)
+    return commodity.order.deliveryDate
+end
--- src/Structures/Order.jl
+++ src/Structures/Order.jl
@@ -1,16 +1,22 @@
 # Order structure to store corresponding metadata
 
-# TODO : transform hashs into shallow copy of objects for easier usage
 struct Order
-    bundle :: Bundle            # bundle to which the order belongs
-    deliveryDate :: Int         # delivery date index in instance time horizon
-    content :: Dict{Commodity, Int}  # order content in packages
+    bundle :: Bundle              # bundle to which the order belongs
+    deliveryDate :: Int           # delivery date index in instance time horizon
+    content :: Vector{Commodity}  # order content in packages
     # Packing properties
-    volume :: Int               # total volume of the order
-    ffdUnits :: Int            # number of trucks used with ffd loading and base capacity
-    giantUnits :: Int          # number of trucks used with giant container loading
-    minPackSize :: Int          # size of the smallest commodity in the order
-    maxPackSize :: Int          # size of the largest commodity in the order
+    volume :: Int                 # total volume of the order
+    ffdUnits :: Int               # number of trucks used with ffd loading and base capacity
+    giantUnits :: Int             # number of trucks used with giant container loading
+    minPackSize :: Int            # size of the smallest commodity in the order
 end
 
 # Methods
+
+function get_supplier(order::Order)
+    return order.bundle.supplier
+end
+
+function get_customer(order::Order)
+    return order.bundle.customer
+end
