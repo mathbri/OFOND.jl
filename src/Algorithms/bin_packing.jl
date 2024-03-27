fe282c023c946f1c7c16810d364f2df3c0233ffb
mathbri <mathis.brichet@gmail.com>
2024-03-27 15:06:04 +0100

    Implementing first fit and best fit heuristics

--- src/Algorithms/bin_packing.jl
+++ src/Algorithms/bin_packing.jl
@@ -1,14 +1,43 @@
 # Bin packing functions
 
-# All functions here should have a parameter to indicate whether the whole packing is 
-# computed or if the curent state of the bins are to remain unchanged
+# TODO : store last size seen to avoid recomputing some find...
 
-function first_fit_decreasing()
-    
+function first_fit_decreasing(bins::Vector{Bin}, capacity::Int, commodities::Vector{Commodity}; sorted::Bool=false)
+    # Sorting commodities in decreasing order of size (if not already done)
+    if !sorted
+        sort!(commodities, by=com -> com.size, rev=true)        
+    end
+    # Adding commodities on top of others
+    for commodity in commodities
+        added = false
+        for bin in bins
+            add!(bin, commodity) && (added = true; break)
+        end
+        added || push!(bins, Bin(capacity - commodity.size, [commodity]))
+    end
 end
 
-function best_fit_decreasing()
-    
+function best_fit_decreasing(bins::Vector{Bin}, capacity::Int, commodities::Vector{Commodity}; sorted::Bool=false)
+    # Sorting commodities in decreasing order of size (if not already done)
+    if !sorted
+        sort!(commodities, by=com -> com.size, rev=true)        
+    end
+    # Storing last size seen to avoid recomputing possible bins
+    lastSizeSeen = commodities[1].size
+    possibleBins = filter(bin -> bin.availableCapacity - commodity.size >= 0, bins)
+    # Adding commodities on top of others
+    for commodity in commodities
+        # Filtering bins with enough space (if needed)
+        if commodity.size != lastSizeSeen
+            possibleBins = filter(bin -> bin.availableCapacity - commodity.size >= 0, bins)
+            lastSizeSeen = commodity.size
+        end
+        # If no bins, adding one and continuing to the next commodity
+        length(possibleBins) == 0 && (push!(bins, Bin(capacity - commodity.size, [commodity])); continue)
+        # Selecting best bin
+        _, bestBin = findmin(bin -> bin.availableCapacity - commodity.size, possibleBins)
+        add!(bin[bestBin], commodity)
+    end
 end
 
 function milp_packing()
--- src/Structures/Bin.jl
+++ src/Structures/Bin.jl
@@ -1,12 +1,22 @@
 # Bin structure used in for bin-packing
 
+# TODO : Changing form mutable to immutable could be a way to improve efficiency
 mutable struct Bin
     idx :: Int                    # index for output purposes
     availableCapacity :: Int      # space left in the bin
     content :: Vector{Commodity}  # which commodity is in the bin
     
-    Bin(availableCapacity, content) = new(availableCapacity, content)
-    Bin(availableCapacity) = new(availableCapacity, Vector{Commodity}())
+    Bin(availableCapacity, content) = new(rand(Int), availableCapacity, content)
+    Bin(availableCapacity) = new(rand(Int), availableCapacity, Vector{Commodity}())
 end
 
 # Methods 
+
+function add!(bin::Bin, commodity::Commodity)
+    if bin.availableCapacity >= commodity.size
+        push!(bin.content, commodity)
+        bin.availableCapacity -= commodity.size
+        return true
+    end
+    return false
+end
