b1bfbf6687f64d64f9bc1d112408bb0d5ce5015c
mathbri <mathis.brichet@gmail.com>
2024-04-03 16:56:49 +0200

    Separating update cost matrix into multiple functions and adding necessary options

--- src/Algorithms/Greedy/greedy_utils.jl
+++ src/Algorithms/Greedy/greedy_utils.jl
@@ -8,54 +8,158 @@ function sort_order_content!(instance::Instance)
     end
 end
 
-# TODO : check that the shotcut computation is actually true
-function get_bundle_update_nodes(travelTimeUtils::TravelTimeUtils, travelTimeGraph::TravelTimeGraph, bundleIdx::Int)
-    startNodes = [travelTimeUtils.bundleStartNodes[bundleIdx]]
-    currentIdx = travelTimeUtils.bundleStartNodes[bundleIdx]
-    while travelTimeGraph.networkArcs[currentIdx, currentIdx + 1].type == :shortcut
-        push!(startNodes, currentIdx + 1)
-        currentIdx += 1
-    end
-    # Other while condition : findfirst(node -> travelTimeGraph.networkArcs[startNode, node].type == :shortcut, outneighbors(travelTimeGraph, startNode)) !== nothing
-    # Other while condition invilves way more computation
-    return vcat(startNodes, travelTimeUtils.commonNodes)
+# Check whether the arc is fit for a cost update
+function is_update_candidate(arcData::NetworkArc, dst::Int, bundleDst::Int)
+    # If it is a shortcut leg, cost alredy set to EPS
+    arcData.type == :shortcut && return false
+    # If the destination is not the right plant, not updating cost
+    (arcData.type == :delivery && dst != bundleDst) && return false
+    return true
 end
 
-# TODO : divide more in multiple functions, too big and too many arguments
-# Updating cost matrix on the travel time graph for a specific bundle using predefined list of nodes to go through
-function update_cost_matrix!(travelTimeUtils::TravelTimeUtils, travelTimeGraph::TravelTimeGraph, updateNodes::Vector{Int}, bundle::Bundle, bundleDst::Int, bundleUtil::BundleUtils, timeSpaceUtils::TimeSpaceUtils; sorted::Bool=sorted)
-    # Iterating through all update nodes and their outneighbors
-    for src in updateNodes
-        for dst in outneighbors(travelTimeGraph, src)
-            dstData = travelTimeGraph.networkNodes[dst]
-            arcData = travelTimeGraph.networkArcs[src, dst]
-            # If it is a shortcut leg, cost alredy set to EPS
-            arcData.type == :shortcut && continue
-            # If the destination is not the right plant, not updating cost
-            (arcData.type == :shortcut && dst != bundleDst) && continue
-            # Adding cost for each order in the bundle
-            arcBundleCost = EPS
-            for (idxO, order) in enumerate(bundle.orders)
-                orderUtil = bundleUtil.orderUtils[idxO]
-                # Node volume cost 
-                arcBundleCost += dstData.volumeCost * orderUtil.volume
-                # Commodity cost 
-                arcBundleCost += arcData.distance * orderUtil.leadTimeCost
-                # Transport cost 
-                if arcData.isLinear
-                    arcBundleCost += (orderUtil.volume / arcData.capacity) * arcData.unitCost
-                    arcBundleCost += orderUtil.giantUnits * arcData.carbonCost
-                else
-                    arcOrderTrucks = orderUtil.bpUnits
-                    # If the arc is not empty, computing a tentative first fit 
-                    if length(timeSpaceUtils.binLoads[src, dst]) > 0
-                        arcOrderTrucks = first_fit_decreasing(timeSpaceUtils.binLoads[src, dst], arcData.capacity, order.content, sorted=sorted)
-                    end
-                    arcBundleCost += arcOrderTrucks * (arcData.unitCost + arcData.carbonCost)
-                end
-            end
-            travelTimeUtils.costMatrix[src, dst] = arcBundleCost
+function get_arc_order_transport_units(
+    timeSpaceGraph::TimeSpaceGraph, 
+    timedSrc::Int, 
+    timedDst::Int, 
+    order::Order;
+    sorted::Bool, 
+    use_bins::Bool
+)
+    arcData = timeSpaceGraph.networkArcs[timedSrc, timedDst]
+    # Transport cost 
+    arcOrderTrucks = get_transport_units(order, arcData)
+    # If we take into account the current solution
+    if use_bins
+        # If the arc is not empty, computing a tentative first fit 
+        if length(timeSpaceGraph.binLoads[timedSrc, timedDst]) > 0
+            arcOrderTrucks = first_fit_decreasing(binOrLoad, arcData.capacity, order.content, sorted=sorted)
         end
+    end
+    return arcOrderTrucks
+end
+
+function get_arc_update_cost(
+    travelTimeGraph::TravelTimeGraph, 
+    timeSpaceGraph::TimeSpaceGraph, 
+    bundle::Bundle, 
+    src::Int, 
+    dst::Int; 
+    sorted::Bool,
+    use_bins::Bool,
+    opening_factor::Float64
+)
+    bundleDst = travelTimeGraph.bundleEndNodes[bundle.idx]
+    dstData, arcData = travelTimeGraph.networkNodes[dst], travelTimeGraph.networkArcs[src, dst]
+    # If the arc doesn't need an update, skipping
+    is_update_candidate(arcData, dst, bundleDst) || return travelTimeGraph.costMatrix[src, dst]
+    # Otherwise, computing the new cost
+    arcBundleCost = EPS
+    for order in bundle.orders
+        # Getting time space projection
+        timedSrc, timedDst = time_space_projector(travelTimeGraph, timeSpaceGraph, src, dst, order.deliveryDate)
+        # Node volume cost 
+        arcBundleCost += dstData.volumeCost * order.volume
+        # Commodity cost 
+        arcBundleCost += arcData.distance * order.leadTimeCost
+        # Arc order cost 
+        arcBundleCost += get_arc_order_transport_units(timeSpaceGraph, timedSrc, timedDst, order, sorted=sorted, use_bins=use_bins) * (arcData.unitCost + arcData.carbonCost) * opening_factor 
+    end
+    return arcBundleCost
+end
+
+# Same function specialized for current cost computation 
+# TODO : find a way to factorize or be very careful when changing one or the other
+function get_arc_update_cost(
+    travelTimeGraph::TravelTimeGraph, 
+    timeSpaceGraph::TimeSpaceGraph, 
+    bundle::Bundle, 
+    src::Int, 
+    dst::Int,
+    currentCost::SparseMatrixCSC{Float64, Int};
+    sorted::Bool,
+    use_bins::Bool,
+    opening_factor::Float64
+)
+    bundleDst = travelTimeGraph.bundleEndNodes[bundle.idx]
+    dstData, arcData = travelTimeGraph.networkNodes[dst], travelTimeGraph.networkArcs[src, dst]
+    # If the arc doesn't need an update, skipping
+    is_update_candidate(arcData, dst, bundleDst) || return travelTimeGraph.costMatrix[src, dst]
+    # Otherwise, computing the new cost
+    arcBundleCost = EPS
+    for order in bundle.orders
+        # Getting time space projection
+        timedSrc, timedDst = time_space_projector(travelTimeGraph, timeSpaceGraph, src, dst, order.deliveryDate)
+        # Node volume cost 
+        arcBundleCost += dstData.volumeCost * order.volume
+        # Commodity cost 
+        arcBundleCost += arcData.distance * order.leadTimeCost
+        # Arc order cost 
+        arcBundleCost += get_arc_order_transport_units(timeSpaceGraph, timedSrc, timedDst, order, sorted=sorted, use_bins=use_bins) * currentCost[timedSrc, timedDst] * opening_factor
+    end
+    return arcBundleCost
+end
+
+# Updating cost matrix on the travel time graph for a specific bundle using predefined list of nodes to go through
+function update_cost_matrix!(
+    travelTimeGraph::TravelTimeGraph, 
+    timeSpaceGraph::TimeSpaceGraph,
+    bundle::Bundle; 
+    sorted::Bool=false, 
+    use_bins::Bool=true,
+    opening_factor::Float64=1.0
+)
+    # Iterating through outneighbors of the start node
+    src = travelTimeGraph.bundleStartNodes[bundle.idx]
+    for dst in outneighbors(travelTimeGraph, src)
+        # Adding cost for each order in the bundle
+        travelTimeUtils.costMatrix[src, dst] = get_arc_update_cost(travelTimeGraph, timeSpaceGraph, bundle, src, dst, sorted=sorted, use_bins=use_bins, opening_factor=opening_factor)
+    end
+    # Iterating through outneighbors of the common nodes
+    for src in travelTimeGraph.commonNodes
+        for dst in outneighbors(travelTimeGraph, src)
+            travelTimeUtils.costMatrix[src, dst] = get_arc_update_cost(travelTimeGraph, timeSpaceGraph, bundle, src, dst, sorted=sorted, use_bins=use_bins, opening_factor=opening_factor)
+        end
+    end
+    # Iterating through outneighbors of the other start node (using while condition on the existence of an outneighbor linked with a shortcut arc)
+    otherSrc = findfirst(node -> travelTimeGraph.networkArcs[startNode, node].type == :shortcut, outneighbors(travelTimeGraph, src))
+    while otherSrc !== nothing
+        src = otherSrc
+        for dst in outneighbors(travelTimeGraph, src)
+            travelTimeUtils.costMatrix[src, dst] = get_arc_update_cost(travelTimeGraph, timeSpaceGraph, bundle, src, dst, sorted=sorted, use_bins=use_bins, opening_factor=opening_factor)
+        end
+        otherSrc = findfirst(node -> travelTimeGraph.networkArcs[startNode, node].type == :shortcut, outneighbors(travelTimeGraph, src))
+    end
+end
+
+function update_cost_matrix!(
+    travelTimeGraph::TravelTimeGraph, 
+    timeSpaceGraph::TimeSpaceGraph,
+    bundle::Bundle,
+    currentCost::SparseMatrixCSC{Float64, Int}; 
+    sorted::Bool=false, 
+    use_bins::Bool=true,
+    opening_factor::Float64=1.0
+)
+    # Iterating through outneighbors of the start node
+    src = travelTimeGraph.bundleStartNodes[bundle.idx]
+    for dst in outneighbors(travelTimeGraph, src)
+        # Adding cost for each order in the bundle
+        travelTimeUtils.costMatrix[src, dst] = get_arc_update_cost(travelTimeGraph, timeSpaceGraph, bundle, src, dst, currentCost, sorted=sorted, use_bins=use_bins, opening_factor=opening_factor)
+    end
+    # Iterating through outneighbors of the common nodes
+    for src in travelTimeGraph.commonNodes
+        for dst in outneighbors(travelTimeGraph, src)
+            travelTimeUtils.costMatrix[src, dst] = get_arc_update_cost(travelTimeGraph, timeSpaceGraph, bundle, src, dst, currentCost, sorted=sorted, use_bins=use_bins, opening_factor=opening_factor)
+        end
+    end
+    # Iterating through outneighbors of the other start node (using while condition on the existence of an outneighbor linked with a shortcut arc)
+    otherSrc = findfirst(node -> travelTimeGraph.networkArcs[startNode, node].type == :shortcut, outneighbors(travelTimeGraph, src))
+    while otherSrc !== nothing
+        src = otherSrc
+        for dst in outneighbors(travelTimeGraph, src)
+            travelTimeUtils.costMatrix[src, dst] = get_arc_update_cost(travelTimeGraph, timeSpaceGraph, bundle, src, dst, currentCost, sorted=sorted, use_bins=use_bins, opening_factor=opening_factor)
+        end
+        otherSrc = findfirst(node -> travelTimeGraph.networkArcs[startNode, node].type == :shortcut, outneighbors(travelTimeGraph, src))
     end
 end
 
