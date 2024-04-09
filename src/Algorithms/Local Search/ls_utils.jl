224a0e1dfbda144c29741f47019a00c4a72ffdb3
mathbri <mathis.brichet@gmail.com>
2024-04-09 15:19:21 +0200

    Adding time space arcs selection, adapting to sparse matrices structure, adding comments, formatting

--- src/Algorithms/Local Search/ls_utils.jl
+++ src/Algorithms/Local Search/ls_utils.jl
@@ -1,49 +1,83 @@
 # Utils function for local search neighborhoods
 
-# TODO : adapt to multiple removals to avoid recomputaions 
-# TODO : adapt to last changes in greedy utils
+# TODO : when solution struct updated, replace paths with solution and get path with the bundle idx
+# Combine all bundles paths in arguments into a sparse matrix indicating the arcs to work with
+function get_bundles_time_space_arcs(
+    timeSpaceGraph::TimeSpaceGraph,
+    travelTimeGraph::TravelTimeGraph,
+    bundles::Vector{Bundle},
+    paths::Vector{Vector{Int}},
+)
+    I, J = Int[], Int[]
+    # For every bundle path and every order in the bundle, adding the timed nodes in the matrix indices
+    for (bundle, path) in zip(bundles, paths)
+        for order in bundle.orders
+            timedPath = time_space_projector(
+                travelTimeGraph, timeSpaceGraph, path, order.deliveryDate
+            )
+            # Without checking overlapping as the combine function will take care of it
+            append!(I, timedPath[1:(end - 1)])
+            append!(J, timedPath[2:end])
+        end
+    end
+    V = ones(Bool, length(I))
+    # Combine function for bools is | by default
+    return sparse(I, J, V)
+end
 
 # Store previous bins before removing commodities from them
-function save_previous_bins(timeSpaceGraph::TimeSpaceGraph, travelTimeGraph::TravelTimeGraph, bundle::Bundle, path::Vector{Int})
-    I, J, oldBins = Int[], Int[], Bin[]
-    previousCost = 0.
-    # For all orders
-    for order in bundle.orders
-        # For all arcs in the path, updating the right bins
-        for (src, dst) in partition(path, 2, 1)
-            timedSrc = time_space_projector(travelTimeGraph, timeSpaceGraph, src, order.deliveryDate)
-            timedDst = time_space_projector(travelTimeGraph, timeSpaceGraph, dst, order.deliveryDate)
-            previousCost += compute_arc_cost(timeSpaceGraph.bins[timedSrc, timedDst], timeSpaceGraph.networkArcs[timedSrc, timedDst], timeSpaceGraph.networkNodes[timedDst])
-            push!(I, timedSrc)
-            push!(J, timedDst)
-            push!(oldBins, timeSpaceGraph.bins[timedSrc, timedDst])
+function save_previous_bins(
+    timeSpaceGraph::TimeSpaceGraph, workingArcs::SparseMatrixCSC{Bool,Int}
+)
+    previousCost = 0.0
+    I, J, _ = findnz(workingArcs)
+    oldBins = Vector{Vector{Bin}}(undef, length(workingArcs))
+    # Efficient iteration over sparse matrices
+    rows = rowvals(workingArcs)
+    for timedDst in 1:size(workingArcs, 2)
+        for srcIdx in nzrange(workingArcs, timedDst)
+            timedSrc = rows[srcIdx]
+            # Adding previous arc cost
+            previousCost += compute_arc_cost(timeSpaceGraph, timedSrc, timedDst)
+            # Storing old bins
+            oldBins[srcIdx] = timeSpaceGraph.bins[timedSrc, timedDst]
         end
     end
     return sparse(I, J, oldBins), previousCost
 end
 
 # Revert the bin loading the the vector of bins given
-function revert_bins!(timeSpaceGraph::TimeSpaceGraph, travelTimeGraph::TravelTimeGraph, bundle::Bundle, path::Vector{Int}, previousBins::SparseMatrixCSC{Vector{Bin}, Int})
-    # For all orders
-    for order in bundle.orders
-        # For all arcs in the path, updating the right bins
-        for (src, dst) in partition(path, 2, 1)
-            timedSrc = time_space_projector(travelTimeGraph, timeSpaceGraph, src, order.deliveryDate)
-            timedDst = time_space_projector(travelTimeGraph, timeSpaceGraph, dst, order.deliveryDate)
+function revert_bins!(
+    timeSpaceGraph::TimeSpaceGraph, previousBins::SparseMatrixCSC{Vector{Bin},Int}
+)
+    # Efficient iteration over sparse matrices
+    rows = rowvals(previousBins)
+    eachindex()
+    for timedDst in 1:size(previousBins, 2)
+        for srcIdx in nzrange(previousBins, timedDst)
+            timedSrc = rows[srcIdx]
+            # Reverting to previous bins
             timeSpaceGraph.bins[timedSrc, timedDst] = previousBins[timedSrc, timedDst]
         end
     end
 end
 
+# TODO : check to see if computing the unique commodities is costful and if the projection here also
 # Remove order content from solution truck loads
-function remove_bundle!(timeSpaceGraph::TimeSpaceGraph, travelTimeGraph::TravelTimeGraph, bundle::Bundle, path::Vector{Int})
+function remove_bundles!(
+    timeSpaceGraph::TimeSpaceGraph,
+    travelTimeGraph::TravelTimeGraph,
+    bundle::Bundle,
+    path::Vector{Int},
+)
     # For all orders
     for order in bundle.orders
+        timedPath = time_space_projector(
+            travelTimeGraph, timeSpaceGraph, path, order.deliveryDate
+        )
         orderUniqueCom = unique(order.content)
         # For all arcs in the path, updating the right bins
-        for (src, dst) in partition(path, 2, 1)
-            timedSrc = time_space_projector(travelTimeGraph, timeSpaceGraph, src, order.deliveryDate)
-            timedDst = time_space_projector(travelTimeGraph, timeSpaceGraph, dst, order.deliveryDate)
+        for (timedSrc, timedDst) in partition(timedPath, 2, 1)
             for bin in timeSpaceGraph.bins[timedSrc, timedDst]
                 filter!(com -> com in orderUniqueCom, bin.content)
             end
@@ -51,56 +85,72 @@ function remove_bundle!(timeSpaceGraph::TimeSpaceGraph, travelTimeGraph::TravelT
     end
 end
 
-function refill_bins!(timeSpaceGraph::TimeSpaceGraph, travelTimeGraph::TravelTimeGraph, bundle::Bundle, path::Vector{Int})
-    costAfterRefill = 0.
-    # For all orders
-    for order in bundle.orders
-        # For all arcs in the path, updating the right bins
-        for (src, dst) in partition(path, 2, 1)
+function remove_bundles!(
+    timeSpaceGraph::TimeSpaceGraph,
+    travelTimeGraph::TravelTimeGraph,
+    bundles::Vector{Bundle},
+    paths::Vector{Vector{Int}},
+)
+    for (bundle, path) in zip(bundles, paths)
+        remove_bundles!(timeSpaceGraph, travelTimeGraph, bundle, path)
+    end
+end
+
+function refill_bins!(
+    timeSpaceGraph::TimeSpaceGraph, workingArcs::SparseMatrixCSC{Bool,Int}
+)
+    costAfterRefill = 0.0
+    # Efficient iteration over sparse matrices
+    rows = rowvals(workingArcs)
+    for timedDst in 1:size(workingArcs, 2)
+        for srcIdx in nzrange(workingArcs, timedDst)
+            timedSrc = rows[srcIdx]
+            # Cathering all remaining commodities than emptying bins
             allCommodities = reduce(vcat, timeSpaceGraph.bins[timedSrc, timedDst])
-            timedSrc = time_space_projector(travelTimeGraph, timeSpaceGraph, src, order.deliveryDate)
-            timedDst = time_space_projector(travelTimeGraph, timeSpaceGraph, dst, order.deliveryDate)
-            costAfterRefill += compute_arc_cost(timeSpaceGraph.bins[timedSrc, timedDst], timeSpaceGraph.networkArcs[timedSrc, timedDst], timeSpaceGraph.networkNodes[timedDst])
             empty!(timeSpaceGraph.bins[timedSrc, timedDst])
-            first_fit_decreasing!(timeSpaceGraph.bins[timedSrc, timedDst], timeSpaceGraph.networkArcs[timedSrc, timedDst].capacity, allCommodities, sorted=sorted)
+            # Filling it back again
+            first_fit_decreasing!(
+                timeSpaceGraph.bins[timedSrc, timedDst],
+                timeSpaceGraph.networkArcs[timedSrc, timedDst].capacity,
+                allCommodities;
+                sorted=false,
+            )
+            # Adding new arc cost
+            costAfterRefill += compute_arc_cost(timeSpaceGraph, timedSrc, timedDst)
         end
     end
     return costAfterRefill
 end
 
-# Compute the update directly on the bins 
-function update_cost_matrix!(travelTimeUtils::TravelTimeUtils, travelTimeGraph::TravelTimeGraph, updateNodes::Vector{Int}, bundle::Bundle, bundleDst::Int, bundleUtil::BundleUtils, timeSpaceUtils::TimeSpaceUtils; sorted::Bool=false)
-    # Iterating through all update nodes and their outneighbors
-    for src in updateNodes
-        for dst in outneighbors(travelTimeGraph, src)
-            dstData = travelTimeGraph.networkNodes[dst]
-            arcData = travelTimeGraph.networkArcs[src, dst]
-            # If it is a shortcut leg, cost alredy set to EPS
-            arcData.type == :shortcut && continue
-            # If the destination is not the right plant, not updating cost
-            (arcData.type == :shortcut && dst != bundleDst) && continue
-            # Adding cost for each order in the bundle
-            arcBundleCost = EPS
-            for (idxO, order) in enumerate(bundle.orders)
-                orderUtil = bundleUtil.orderUtils[idxO]
-                # Node volume cost 
-                arcBundleCost += dstData.volumeCost * orderUtil.volume
-                # Commodity cost 
-                arcBundleCost += arcData.distance * orderUtil.leadTimeCost
-                # Transport cost 
-                if arcData.isLinear
-                    arcBundleCost += (orderUtil.volume / arcData.capacity) * arcData.unitCost
-                    arcBundleCost += orderUtil.giantUnits * arcData.carbonCost
-                else
-                    arcOrderTrucks = orderUtil.bpUnits
-                    # If the arc is not empty, computing a tentative first fit 
-                    if length(timeSpaceUtils.binLoads[src, dst]) > 0
-                        arcOrderTrucks = first_fit_decreasing(timeSpaceGraph.bins[src, dst], arcData.capacity, order.content, sorted=sorted)
-                    end
-                    arcBundleCost += arcOrderTrucks * (arcData.unitCost + arcData.carbonCost)
-                end
-            end
-            travelTimeUtils.costMatrix[src, dst] = arcBundleCost
-        end
+function select_two_nodes(travelTimeGraph::TravelTimeGraph)
+    node1 = rand(keys(travelTimeGraph.bundlesOnNodes))
+    node2 = rand(keys(travelTimeGraph.bundlesOnNodes))
+    while node1 == node2
+        node2 = rand(keys(travelTimeGraph.bundlesOnNodes))
+    end
+    return node1, node2
+end
+
+function get_bundles_to_update(travelTimeGraph::TravelTimeGraph, node1::Int, node2::Int=-1)
+    node2 == -1 && return travelTimeGraph.bundlesOnNodes[node1]
+    return intersect(
+        travelTimeGraph.bundlesOnNodes[node1], travelTimeGraph.bundlesOnNodes[node2]
+    )
+end
+
+function update_bundle_paths!(
+    bundlePaths::Vector{Vector{Int}},
+    src::Int,
+    dst::Int,
+    bundles::Vector{Bundle},
+    paths::Vector{Vector{Int}},
+)
+    for bundle in bundles
+        bundlePath = bundlePaths[bundle.idx]
+        srcIdx = findfirst(node -> node == src, bundlePath)
+        dstIdx = findlast(node -> node == dst, bundlePath)
+        bundlePaths[bundle.idx] = vcat(
+            bundlePath[1:srcIdx], paths[bundle.idx][2:(end - 1)], bundlePath[dstIdx:end]
+        )
     end
 end
