5a5742caf136b063364475db1e8d7539f1972dec
mathbri <mathis.brichet@gmail.com>
2024-04-10 15:44:28 +0200

    first implem of milp nieghbor functions, missing three

--- src/Algorithms/LNS/lns_utils.jl
+++ src/Algorithms/LNS/lns_utils.jl
@@ -22,61 +22,167 @@ function slope_scaling_cost_update!(timeSpaceGraph::TimeSpaceGraph)
     end
 end
 
-# TODO : At least a function to build the milp based on the bundles
-# Could be good idea to have a function if there is one point (plant neighbor) and two nodes (two node neighbor or attract reduce neighbor)
+# TODO : e is a matrix B x V, construct it sparse, depend whether all bundles have same source and dest or just same source or none
+function get_e_matrix() end
 
-# TODO : finish this function and make sur the indices and all corresponds
-# define packing variable only on the consolidated arcs
-# define direct and outsource bundle costs
-# add all corresponding arguments : bundles concerned, travel time graph, time space graph, start end nodes, ...
-# detect whether the path and packing constraints don't have any variables inside ? unlickily because path constraints on all the travel time graph...
-
-function create_neighborhood_milp()
-    # model = Model(optimizer_with_attributes(() -> Gurobi.Optimizer(GRB_ENV), "OutputFlag" => 0))
-    model = Model(HiGHS.Optimizer)
-
-    # Variables
-    nBundles, nArcsTT, nArcsTS = length(bundles),
-    ne(travelTimeGraph.graph),
-    ne(timeSpaceGraph.graph)
-    # Variable x[b, a] in {0, 1} indicate if bundle b uses arc a in the travel time graph
-    @variable(model, x[1:nBundles, 1:nArcsTT], Bin)
-    # Varible tau_a indicate the number of transport units used on arc a in the time space graph
-    @variable(model, tau[1:nArcsTS], Int)
-
-    # Constraints
-    # TODO : missing the node argument in here
-    # Path constraints on the travel time graph
+# Add path constraints on the travel time graph to the model
+function add_path_constraints!(
+    model::Model,
+    travelTimeGraph::TravelTimeGraph,
+    bundles::Vector{Bundle},
+    e::SparseMatrixCSC{Int,Int},
+)
     incidence_matrix = incidence_matrix(travelTimeGraph.graph)
-    @constraint(model, path[b=1:nBundles], incidence_matrix * x[b, :] .== startEndVector[b])
-    # Relaxed packing constraint on the time space graph
-    for arc in edges(timeSpaceGraph.graph)
-        expr = AffExpr(timeSpaceGraph.bins[a], tau[a] => -timeSpaceGraph.unitCapacity[a])
-        for bundle in bundles
-            travelTimeSources, travelTimesDests = travel_time_projector(
-                travelTimeGraph, timeSpaceGraph, arc, bundle
-            )
-            for (idxO, order) in enumerate(bundle.orders)
-                src, dst = travelTimeSources[idxO], travelTimesDests[idxO]
-                src != -1 &&
-                    dst != -1 &&
-                    add_to_expression!(expr, x[bundle.idx, (src, dst)], order.volume)
+    bundlesIndexes = [bundle.idx for bundle in bundles]
+    x = model[:x]
+    @constraint(model, path[b in bundlesIndexes], incidence_matrix * x[b, :] .== e[b, :])
+end
+
+# Initialize the packing expression as sparse matrix with space used and tau variables
+function init_packing_expr(model::Model, timeSpaceGraph::TimeSpaceGraph)
+    I, J, E, tau = Int[], Int[], AffExpr[], model[:tau]
+    for arc in timeSpaceGraph.commonArcs
+        expr = AffExpr(
+            sum(timeSpaceGraph.binLoads[src(arc), dst(arc)]),
+            tau[arc] => -timeSpaceGraph.networkArcs[src(arc), dst(arc)].capacity,
+        )
+        push!(I, src(arc))
+        push!(J, dst(arc))
+        push!(E, expr)
+    end
+    return sparse(I, J, E)
+end
+
+# Add all relaxed packing constraints on shared arc of the time space graph
+function add_packing_constraints!(
+    model::Model,
+    travelTimeGraph::TravelTimeGraph,
+    timeSpaceGraph::TimeSpaceGraph,
+    bundles::Vector{Bundle},
+)
+    x = model[:x]
+    packing_expr = init_packing_expr(model, timeSpaceGraph)
+    for bundle in bundles
+        for order in bundle.orders
+            for arc in timeSpaceGraph.commonArcs
+                # Projecting back on the travel time graph
+                ttSrc, ttDst = travel_time_projector(
+                    travelTimeGraph, timeSpaceGraph, src(arc), dst(arc), order
+                )
+                # Checking the arc is for the bundle
+                (ttSrc == -1 || ttDst == -1) || continue
+                # Add the corresponding bundle variable and order volume to the expression
+                add_to_expression!(
+                    packing_expr[src(arc), dst(arc)],
+                    x[bundle.idx, (ttSrc, ttDst)],
+                    order.volume,
+                )
             end
         end
-        @constraint(model, packing[arc], expr <= 0)
     end
+    @constraint(
+        model,
+        packing[arc in timeSpaceGraph.commonArcs],
+        packing_expr[src(arc), dst(arc)] <= 0
+    )
+end
+
+# Check if the arc is a direct or outsource one
+function is_direct_outsource(travelTimeGraph::TravelTimeGraph, arc::Edge)
+    arcData = travelTimeGraph.networkArcs[src(arc), dst(arc)]
+    return arcData.type == :direct || arcData.type == :outsource
+end
+
+# Compute cost as giant container for direct and linear for others
+function get_arc_milp_cost(
+    TTGraph::TravelTimeGraph, TSGraph::TimeSpaceGraph, bundle::Bundle, src::Int, dst::Int
+)
+    arcData = TTGraph.networkArcs[src, dst]
+    arcBundleCost = EPS
+    for order in bundle.orders
+        # Getting time space projection
+        timedSrc, timedDst = time_space_projector(
+            TTGraph, TSGraph, src, dst, order.deliveryDate
+        )
+        # Node volume cost 
+        arcBundleCost += get_order_node_com_cost(TTGraph, src, dst, order)
+        # Arc transport cost 
+        arcBundleCost +=
+            get_lb_transport_units(order, arcData) * TSGraph.currentCost[timedSrc, timedDst]
+    end
+    return arcBundleCost
+end
+
+# Computes direct and outsource cost
+function get_direct_outsource_cost(
+    TTGraph::TravelTimeGraph, TSGraph::TimeSpaceGraph, bundles::Vector{Bundle}
+)
+    cost_matrix = zeros(length(bundles), ne(TTGraph.graph))
+    for (a, arc) in enumerate(edges(TTGraph.graph))
+        # If the arc is not direct or outsource, cost is 0 (handled by tau variables)
+        if !is_direct_outsource(TTGraph, arc)
+            cost_matrix[:, a] .= 0
+            continue
+        end
+        # Otherwise, computing giant container for direct and linear for outsource
+        for (b, bundle) in enumerate(bundles)
+            cost_matrix[b, a] = get_arc_milp_cost(
+                TTGraph, TSGraph, bundle, src(arc), dst(arc);
+            )
+        end
+    end
+    return cost_matrix
+end
+
+# Sums the current cost of shared timed arcs and pre-computed surect and outsource arcs
+function get_milp_objective(
+    model::Model,
+    travelTimeGraph::TravelTimeGraph,
+    timeSpaceGraph::TimeSpaceGraph,
+    bundles::Vector{Bundle},
+)
+    x, tau = model[:x], model[:tau]
+    return sum(
+        timeSpaceGraph.currentCost[src(arc), dst(arc)] * tau[arc] for
+        arc in timeSpaceGraph.commonArcs
+    ) + sum(
+        get_direct_outsource_cost(
+            travelTimeGraph::TravelTimeGraph,
+            timeSpaceGraph::TimeSpaceGraph,
+            bundles::Vector{Bundle},
+        ) .* x,
+    )
+end
+
+function create_neighborhood_milp(
+    travelTimeGraph::TravelTimeGraph,
+    timeSpaceGraph::TimeSpaceGraph,
+    bundles::Vector{Bundle},
+)
+    model = Model(HiGHS.Optimizer)
+
+    # Variable x[b, a] in {0, 1} indicate if bundle b uses arc a in the travel time graph
+    @variable(
+        model, x[[bundle.idx for bundle in bundles], edges(travelTimeGraph.graph)], Bin
+    )
+    # Varible tau_a indicate the number of transport units used on arc a in the time space graph
+    @variable(model, tau[timeSpaceGraph.commonArcs], Int)
+
+    # Path constraints on the travel time graph
+    e = get_e_matrix()
+    add_path_constraints!(model, travelTimeGraph, bundles, e)
+    # Relaxed packing constraint on the time space graph
+    add_packing_constraints!(model, travelTimeGraph, timeSpaceGraph, bundles)
 
     # Objective
     @objective(
-        model,
-        Min,
-        sum(timeSpaceGraph.currentCost[a] * tau[a] for a in 1:nArcsTS) +
-            sum(directOrOutsourceCost[b, a] * x[b, a] for b in 1:nBundles, a in 1:nArcsTT)
+        model, Min, get_milp_objective(model, travelTimeGraph, timeSpaceGraph, bundles)
     )
-
     # Returning the constructed model
     return model
 end
+
+# TODO : add a function to add constraints fot he path formulation ? or directly a new milp ?
 
 function solve_milp()
     # Options
