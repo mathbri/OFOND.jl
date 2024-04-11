70ad6123f0547ea6ce250f2a6b17fb4bed50db34
mathbri <mathis.brichet@gmail.com>
2024-04-11 17:07:27 +0200

    Finishing time space factorization and adding projector file

--- src/Structures/TimeSpace.jl
+++ src/Structures/TimeSpace.jl
@@ -3,142 +3,134 @@
 # Time Space Graph
 struct TimeSpaceGraph
     # Core fields
-    graph :: SimpleDiGraph
-    timeHorizon :: Int
-    networkNodes :: Vector{NetworkNode}
-    timeSteps :: Vector{Int}
-    networkArcs :: SparseMatrixCSC{NetworkArc, Int}
-    bins :: SparseMatrixCSC{Vector{Bin}, Int}
+    graph::SimpleDiGraph # graph structure
+    timeHorizon::Int # number of steps in the time horizon
+    networkNodes::Vector{NetworkNode} # nodes data
+    timeStep::Vector{Int} # time steps for each node
+    networkArcs::SparseMatrixCSC{NetworkArc,Int} # arcs data
     # Properties
-    travelTimeLink :: Dict{UInt, Int}                # dict to easily recover nodes from travel time to time space
-    currentCost :: SparseMatrixCSC{Float64, Int}   # used by slope scaling
-    binLoads :: SparseMatrixCSC{Vector{Int}, Int}  # bin loads on this arc (used for fastier computation)
+    hashToIdx::Dict{UInt,Int}                # dict to easily recover nodes from travel time to time space
+    currentCost::SparseMatrixCSC{Float64,Int}   # used by slope scaling
+    commonArcs::Vector{Edge} # common arcs of the graph
+end
+
+function TimeSpaceGraph(timeHorizon::Int)
+    return TimeSpaceGraph(
+        DiGraph(),
+        timeHorizon,
+        NetworkNode[],
+        Int[],
+        SparseMatrixCSC{NetworkArc,Int}(),
+        Dict{UInt,Int}(),
+        sparse(zeros(Float64, 0, 0)),
+        Edge[],
+    )
+end
+
+function TimeSpaceGraph()
+    return TimeSpaceGraph(0)
+end
+
+function TimeSpaceGraph(
+    timeSpaceGraph::TimeSpaceGraph,
+    I::Vector{Int},
+    J::Vector{Int},
+    arcs::Vector{NetworkArc},
+    costs::Vector{Float64},
+)
+    return TimeSpaceGraph(
+        timeSpaceGraph.graph,
+        timeSpaceGraph.timeHorizon,
+        timeSpaceGraph.networkNodes,
+        timeSpaceGraph.timeStep,
+        sparse(I, J, arcs),
+        timeSpaceGraph.hashToIdx,
+        sparse(I, J, costs),
+        timeSpaceGraph.commonArcs,
+    )
 end
 
 # Methods
 
-# TODO : put all major block in functions
-function build_time_space_and_utils(network::NetworkGraph, timeHorizon::Int)
+function add_network_node!(timeSpaceGraph::TimeSpaceGraph, node::NetworkNode)
+    # Adding a timed copy for each time step 
+    for timeStep in 1:(timeSpaceGraph.timeHorizon)
+        # Adding timed copy to the graph
+        add_vertex!(timeSpaceGraph.graph)
+        push!(timeSpaceGraph.networkNodes, node)
+        push!(timeSpaceGraph.timeStep, timeStep)
+        timeSpaceGraph.hashToIdx[hash(timeStep, node.hash)] = nv(timeSpaceGraph.graph)
+    end
+end
+
+function add_network_arc!(
+    timeSpaceGraph::TimeSpaceGraph,
+    srcData::NetworkNode,
+    dstData::NetworkNode,
+    arcData::NetworkArc,
+)
+    for timeStep in 1:(timeSpaceGraph.timeHorizon)
+        # Adding timed copy of network arc
+        src = get(timeSpaceGraph.hashToIdx, hash(timeStep, srcData.hash), nothing)
+        dst = get(
+            timeSpaceGraph.hashToIdx,
+            hash(timeStep + arcData.travelTime, dstData.hash),
+            nothing,
+        )
+        if src !== nothing && dst !== nothing
+            add_edge!(timeSpaceGraph.graph, src, dst)
+        end
+    end
+end
+
+function add_arc_to_vectors!(
+    vectors::Tuple{Vector{Int},Vector{Int},Vector{NetworkArc},Vector{Float64}},
+    timeSpaceGraph::TimeSpaceGraph,
+    srcData::NetworkNode,
+    dstData::NetworkNode,
+    arcData::NetworkArc,
+)
+    for timeStep in 1:(timeSpaceGraph.timeHorizon)
+        # Adding timed copy of network arc
+        src = get(travelTimeGraph.hashToIdx, hash(timeStep, srcData.hash), nothing)
+        dst = get(
+            travelTimeGraph.hashToIdx,
+            hash(timeStep + arcData.travelTime, dstData.hash),
+            nothing,
+        )
+        if src !== nothing && dst !== nothing
+            I, J, arcs, costs = vectors
+            push!(I, src)
+            push!(J, dst)
+            push!(arcs, arcData)
+            push!(costs, EPS)
+        end
+    end
+end
+
+function TimeSpaceGraph(network::NetworkGraph, timeHorizon::Int)
     # Initializing structures
-    timeSpaceGraph = TimeSpaceGraph(DiGraph(), timeHorizon, NetworkNode[], Int[], sparse(zeros(Int, 0, 0)), sparse(zeros(Int, 0, 0)), Dict{UInt, Int}())
-    timeSpaceUtils = TimeSpaceUtils(sparse(zeros(Float64, 0, 0)), sparse(zeros(Int, 0, 0)))
+    timeSpaceGraph = TimeSpaceGraph()
     # Adding all nodes from the network graph
     for nodeHash in labels(network)
-        nodeData = network[nodeHash]
-        # Adding a timed copy for each time step 
-        for timeStep in 1:timeHorizon
-            # Adding timed copy to the graph
-            add_vertex!(timeSpaceGraph.graph)
-            push!(timeSpaceGraph.networkNodes, nodeData)
-            push!(timeSpaceGraph.timeSteps, timeStep)
-            timeSpaceGraph.travelTimeLink[hash(timeStep, hash(nodeData))] = nv(timeSpaceGraph.graph)
-        end
+        add_network_node!(timeSpaceGraph, network[nodeHash])
     end
     # Initializing vectors for sparse matrices
-    I, J = Int[], Int[]
-    arcs, bins, costs, loads = NetworkArc[], Vector{Vector{Bin}}(), Float64[], Vector{Vector{Int}}()
+    I, J, arcs, costs = Int[], Int[], NetworkArc[], Float64[]
     nodesHash = hash.(timeSpaceGraph.networkNodes)
-    # Adding all arcs form the network graph
+    # Adding all arcs form the network graph (except shortcuts)
     for (sourceHash, destHash) in edge_labels(network)
-        arcData = network[sourceHash, destHash]
-        # I get all source node copies and dest node copies (via hash)
-        sourceNodeIdxs = findall(nodeHash -> nodeHash == sourceHash, nodesHash)
-        destNodeIdxs = findall(nodeHash -> nodeHash == destHash, nodesHash)
-        # I add an arc when source step to del - arc travel time = dest step to del
-        for sourceNodeIdx in sourceNodeIdxs, destNodeIdx in destNodeIdxs
-            if timeSpaceGraph.stepToDel[sourceNodeIdx] - arcData.travelTime == timeSpaceGraph.stepToDel[destNodeIdx]
-                push!(I, sourceNodeIdx)
-                push!(J, destNodeIdx)
-                push!(arcs, arcData)
-                push!(costs, EPS)
-                push!(loads, Int[])
-                push!(bins, Bin[])
-            end
-        end
+        srcData, dstData, arcData = network[srcHash],
+        network[dstHash],
+        network[srcHash, dstHash]
+        # Not adding shortcut arcs 
+        arcData.type == :shortcut && continue
+        add_network_arc!(timeSpaceGraph, srcData, dstData, arcData)
+        add_arc_to_vectors!((I, J, arcs, costs), timeSpaceGraph, srcData, dstData, arcData)
     end
-    # Building sparse matrix
-    arcMatrix = sparse(I, J, arcs)
-    costMatrix = sparse(I, J, costs)
-    loadMatrix = sparse(I, J, loads)
-    binMatrix = sparse(I, J, bins)
-    # Creating final structures
-    finalTimeSPace = TimeSpaceGraph(timeSpaceGraph.graph, timeSpaceGraph.timeHorizon, timeSpaceGraph.networkNodes, timeSpaceGraph.timeSteps, arcMatrix, binMatrix, timeSpaceGraph.travelTimeLink)
-    finalTimeSpaceUtils = TimeSpaceUtils(costMatrix, loadMatrix)
-    return finalTimeSPace, finalTimeSpaceUtils
+    return TimeSpaceGraph(timeSpaceGraph, I, J, arcs, costs)
 end
 
-# Project a node of the travel time graph on the time space graph for a specific delivery date 
-function time_space_projector(travelTimeGraph::TravelTimeGraph, timeSpaceGraph::TimeSpaceGraph, travelTimeNode::Int, deliveryDate::Int)
-    # Computing the time step from the delivery date and the steps from delivery
-    timeSpaceDate = deliveryDate - travelTimeGraph.stepToDel[travelTimeNode]
-    # If it goes out of the horizon, rolling it back to the top
-    timeSpaceDate < 1 && timeSpaceDate += timeSpaceGraph.timeHorizon
-    # Using travel time link dict to return the right node idx
-    return timeSpaceGraph.travelTimeLink[
-        hash(timeSpaceDate, travelTimeGraph.networkNodes[travelTimeNode].hash)
-    ]    
-end
-
-# Project a arc of the travel time graph on the time space graph for a specific delivery date 
-function time_space_projector(travelTimeGraph::TravelTimeGraph, timeSpaceGraph::TimeSpaceGraph, travelTimeSource::Int, travelTimeDest::Int, deliveryDate::Int)
-    return (
-        time_space_projector(travelTimeGraph, timeSpaceGraph, travelTimeSource, deliveryDate), 
-        time_space_projector(travelTimeGraph, timeSpaceGraph, travelTimeDest, deliveryDate)
-    )
-end
-
-function is_path_elementary(path::Vector{UInt})
-    if length(path) >= 4
-        for (nodeIdx, nodeHash) in enumerate(path)
-            if nodeHash in path[nodeIdx+1:end]
-                # println("Non elementary path found : $path")
-                return false
-            end
-        end
-    end
-    return true
-end
-
-# TODO : adapt from here
-
-# Initialize empty time space graph
-function TimeSpaceGraph(timeHorizon::Int, relaxed::Bool)
-    arcTypeData = relaxed ? RelaxedTimedArc : TimedArc
-    timeSpace = MetaGraph(
-        DiGraph();
-        label_type = UInt,
-        vertex_data_type = TimeSpaceNode,
-        edge_data_type = arcTypeData,
-    )
-    return TimeSpaceGraph(timeSpace, timeHorizon)
-end
-
-function build_time_extended_graph(network::MetaGraph, timeHorizon::Int) :: MetaGraph
-    timeSpaceGraph = TimeSpaceGraph(timeHorizon)
-    println("Building time space graph...")
-    # Adding every (node, time) pair to the graph
-    for nodeHash in labels(network)
-        nodeData = network[nodeHash] 
-        for timeStep in 1:timeHorizon
-            tsNode = TimeSpaceNode(nodeData.account, nodeData.type, timeStep)
-            timeSpace[hash(tsNode)] = tsNode
-            @assert hash(tsNode) == hash(timeStep, hash(nodeData))
-        end
-    end
-    # Adding every timed edges to the graph
-    for (sourceHash, destHash) in edge_labels(network)
-        arcData = network[sourceHash, destHash]
-        @assert arcData.travelTime < timeHorizon
-        # Linking (node, t) with (node, t + travelTime) for every possible step
-        for sourceStep in 1:timeHorizon
-            tsSourceHash = hash(sourceStep, sourceHash)
-            destStep = sourceStep + arcData.travelTime
-            # Correcting step if it is out of the time horizon
-            destStep = (destStep > T) ? (destStep % T) : destStep
-            tsDestHash = hash(sourceStep + arcData.travelTime, destHash)
-            tsArc = TimeSpaceArc(arcData.capacity, Int[], Bin[])
-            timeSpace[ttSourceHash, ttDestHash] = tsArc
-        end
-    end
-    return timeSpace
+function is_path_elementary(timeSpaceGraph::TimeSpaceGraph, path::Vector{Int})
+    return is_path_elementary(hash.(timeSpaceGraph.networkNodes[path]))
 end
---
+++ src/Structures/projectors.jl
@@ -0,0 +1,88 @@
+# Projectors fnctions between travel time and time space
+
+function get_node_step_to_delivery(
+    timeSpaceGraph::TimeSpaceGraph, timeSpaceNode::Int, order::Order
+)
+    # Computing the steps to delivery to know which travel time node should be used for the order
+    stepToDel = order.deliveryDate - timeSpaceGraph.timeSteps[timeSpaceNode]
+    # If the step to delivery is negative, adding the time horizon to it
+    stepToDel < 0 && (stepToDel += timeSpaceGraph.timeHorizon)
+    return stepToDel
+end
+
+# Project a node of the time space graph on the travel time graph for a specific order
+# return -1 if the node time step is after the order delivery date or if the step to delivery is greater than the maximum delivery time 
+function travel_time_projector(
+    travelTimeGraph::TravelTimeGraph,
+    timeSpaceGraph::TimeSpaceGraph,
+    timeSpaceNode::Int,
+    order::Order,
+)
+    # If the time step is after the order delivery date, return -1
+    timeSpaceGraph.timeSteps[timeSpaceNode] > order.deliveryDate && return -1
+    # If the step to delivery is greater than the max delivery time, return -1
+    stepToDel = get_node_step_to_delivery(timeSpaceGraph, timeSpaceNode, order)
+    stepToDel > order.bundle.maxDeliveryTime && return -1
+    # Using time space link dict to return the right node idx
+    return travelTimeGraph.hashToIdx[hash(
+        stepToDel, timeSpaceGraph.networkNodes[timeSpaceNode].hash
+    )]
+end
+
+function travel_time_projector(
+    travelTimeGraph::TravelTimeGraph,
+    timeSpaceGraph::TimeSpaceGraph,
+    timeSpaceSource::Int,
+    timeSpaceDest::Int,
+    order::Order,
+)
+    return (
+        travel_time_projector(travelTimeGraph, timeSpaceGraph, timeSpaceSource, order),
+        travel_time_projector(travelTimeGraph, timeSpaceGraph, timeSpaceDest, order),
+    )
+end
+
+# Project a node of the travel time graph on the time space graph for a specific delivery date 
+function time_space_projector(
+    travelTimeGraph::TravelTimeGraph,
+    timeSpaceGraph::TimeSpaceGraph,
+    travelTimeNode::Int,
+    deliveryDate::Int,
+)
+    # Computing the time step from the delivery date and the steps from delivery
+    timeSpaceDate = deliveryDate - travelTimeGraph.stepToDel[travelTimeNode]
+    # If it goes out of the horizon, rolling it back to the top
+    timeSpaceDate < 1 && timeSpaceDate += timeSpaceGraph.timeHorizon
+    # Using travel time link dict to return the right node idx
+    nodeData = travelTimeGraph.networkNodes[travelTimeNode]
+    return timeSpaceGraph.hashToIdx[hash(timeSpaceDate, nodeData.hash)]
+end
+
+# Project an arc of the travel time graph on the time space graph for a specific delivery date 
+function time_space_projector(
+    travelTimeGraph::TravelTimeGraph,
+    timeSpaceGraph::TimeSpaceGraph,
+    travelTimeSource::Int,
+    travelTimeDest::Int,
+    deliveryDate::Int,
+)
+    return (
+        time_space_projector(
+            travelTimeGraph, timeSpaceGraph, travelTimeSource, deliveryDate
+        ),
+        time_space_projector(travelTimeGraph, timeSpaceGraph, travelTimeDest, deliveryDate),
+    )
+end
+
+# Project a path of the travel time graph on the time space graph for a specific delivery date 
+function time_space_projector(
+    travelTimeGraph::TravelTimeGraph,
+    timeSpaceGraph::TimeSpaceGraph,
+    travelTimePath::Vector{Int},
+    deliveryDate::Int,
+)
+    # Refs are used here to broadcat operation only on the path
+    return time_space_projector.(
+        Ref(travelTimeGraph), Ref(timeSpaceGraph), travelTimePath, Ref(deliveryDate)
+    )
+end
